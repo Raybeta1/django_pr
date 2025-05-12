6e3a47e56f56b9f06e6399a549fcadb54352586d
Raybeta1 <shantanusrivastava895@gmail.com>
2025-05-12 18:57:41 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Blog Home Page - It Works!")
+
+def about(request):
+    return HttpResponse("About Page - Learn more about us here!")
